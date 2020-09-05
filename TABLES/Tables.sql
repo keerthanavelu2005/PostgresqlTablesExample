96237559ee4037ea6beaf1f6cc0e6e9750180c75
Keerthana Velu <67904788+keerthanavelu2005@users.noreply.github.com>
2020-09-05 23:01:54 +0530

    Update Tables.sql

--- TABLES/Tables.sql
+++ TABLES/Tables.sql
@@ -32,7 +32,7 @@ VALUES('keerthana', 'tenth', 'need to develop your skills');
 create table products(
 pid serial not null,
 pname varchar(20),
-price numeric(6,2)
+price numeric CONSTRAINT positive_price CHECK (price>0)
 );
 
 insert into products(pname, price)
@@ -41,15 +41,15 @@ values ('soap', 345.90);
 // Array//
 
 create table employee_sal(
-empid BIT(8),
+empid integer (10),
 empname varchar(20),
 pay_by_quarter integer[],
 phonenum integer[],
-salary numeric (7,2)
+salary numeric (7,2) 
 );
 
 insert into employee_sal
-values('10101010', 'keerthana', '{10000, 10000, 100000, 10000, 10000, 10000}',
+values('101', 'keerthana', '{10000, 10000, 100000, 10000, 10000, 10000}',
 ' {1234567890} ', 20000.45
 );
 
